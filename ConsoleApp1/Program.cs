29c329d5eb4c67b1eb5c68a56061777c13a0a48c
Amadejus Valionis <amadeus444@gmail.com>
2020-09-14 19:53:04 +0300

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i, k, j;
+            Console.WriteLine("Enter the length of the longest wanted starline:");
+            int num = int.Parse(Console.ReadLine());
+
+            for(i = 1; i<=num; num--)
+            {
+                if(num % 2 != 0)
+                {
+                    for(j=1; j<num; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for(k=1; k<=num-i+1; k++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+                
+                
+
+            
+        }
+    }
+}
